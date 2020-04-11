8d9911dc37cef5d4dcd2a08ef8798a60f4cb1c23
Felicia Wieland <felicia.madlen.wieland@hs-furtwangen.de>
2020-04-11 18:03:42 +0200

    javadoc Eisenmetalle

--- Eisenmetalle.java
+++ Eisenmetalle.java
@@ -1,13 +1,15 @@
 
 /**
- * Beschreiben Sie hier die Klasse Eisenmetalle.
+ * Die Klasse Eisenmetalle ist eine abstrakte Subklasse von "Werkstoffe", initialisiert das Attribut Eisengehalt und enthält
+ * sowohl eine get- und set-Methode für dieses Attribut als auch die Methode gibEigenschaft.
  * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
+ * @author Markus Schnee, Jenny Glönkler 
+ * @version 11. April 2020
  */
 public abstract class Eisenmetalle extends Metalle
 {
     private double eisengehalt;
+    
     public Eisenmetalle(String name,int nummer,String verwendung,double elektrischeLeitfaehigkeit,double eisengehalt)
     {
         super(name,nummer,verwendung,elektrischeLeitfaehigkeit);
@@ -16,6 +18,7 @@ public abstract class Eisenmetalle extends Metalle
             this.eisengehalt= eisengehalt;
         }
     }
+    
     public void setEisengehalt(double eisengehalt)
     {
         if(eisengehalt>0)
@@ -23,6 +26,7 @@ public abstract class Eisenmetalle extends Metalle
             this.eisengehalt= eisengehalt;
         }
     }
+    
     public double getEisengehalt()
     {
        return eisengehalt;
