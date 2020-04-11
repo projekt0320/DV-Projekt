fd2d1d671dbf43c405dc631cf88d056217596e6e
Felicia Wieland <felicia.madlen.wieland@hs-furtwangen.de>
2020-04-11 18:05:25 +0200

    javadoc Nichteisenmetalle

--- Nichteisenmetalle.java
+++ Nichteisenmetalle.java
@@ -1,13 +1,15 @@
 
 /**
- * Beschreiben Sie hier die Klasse Nichteisenmetalle.
+ * Die Klasse Nichteisenetalle ist eine abstrakte Subklasse von "Werkstoffe", initialisiert das Attribut Hauptelement
+ * und enthält sowohl eine get- und set-Methode für dieses Attribut als auch die Methode gibEigenschaft.
  * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
+ * @author Markus Schnee, Jenny Glönkler
+ * @version 11. April 2020
  */
 public abstract class Nichteisenmetalle extends Metalle
 {
     private String Hauptelement;
+    
     public Nichteisenmetalle(String name,int nummer,String verwendung,double elektrischeLeitfaehigkeit, String Hauptelement)
     {
         super(name,nummer,verwendung,elektrischeLeitfaehigkeit);
