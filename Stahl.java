198ffd6850fa7c39cc2e0971681d1acc3ee5bebc
Felicia Wieland <felicia.madlen.wieland@hs-furtwangen.de>
2020-04-11 18:08:16 +0200

    Stahl javadoc

--- Stahl.java
+++ Stahl.java
@@ -1,9 +1,11 @@
 
 /**
- * Beschreiben Sie hier die Klasse Stahl.
+ * Die Klasse Stahl ist eine Subklasse von "Werkstoffe", initialisiert das Attribut Kohlenstoffgehalt und enthält
+ * sowohl eine get- und set-Methode für dieses Attribut als auch die Methode gibEigenschaft, mit der alle Eigenschaften und deren Werte
+ * als Text zurückgegeben werden.
  * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
+ * @author Markus Schnee, Jenny Glönkler 
+ * @version 11. April 2020
  */
 public class Stahl extends Eisenmetalle
 {
@@ -22,6 +24,7 @@ public class Stahl extends Eisenmetalle
             throw new Exception ("Ungültiger Kohlenstoffgehalt.");
         }
     }
+    
     public void setKohlenstoffgehalt(double Kohlenstoffgehalt)
     {
         if(Kohlenstoffgehalt>0 && Kohlenstoffgehalt< 100)
@@ -29,10 +32,12 @@ public class Stahl extends Eisenmetalle
             this.Kohlenstoffgehalt= Kohlenstoffgehalt;
         }
     }
+    
     public double getKohlenstoffgehalt()
     {
        return Kohlenstoffgehalt;
     }
+    
     public String gibEigenschaft()
     {
         String s= super.gibEigenschaft()+"\n Kohlenstoffgehalt:   "+Kohlenstoffgehalt;
