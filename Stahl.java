868209f13db815edff0279afb78b6e81e878b7aa
Felicia Wieland <felicia.madlen.wieland@hs-furtwangen.de>
2020-04-25 14:02:07 +0200

    Standardkonstruktor Stahl

--- Stahl.java
+++ Stahl.java
@@ -4,16 +4,15 @@
  * sowohl eine get- und set-Methode für dieses Attribut als auch die Methode gibEigenschaft, mit der alle Eigenschaften und deren Werte
  * als Text zurückgegeben werden.
  * 
- * @author Markus Schnee, Jenny Glönkler 
- * @version 11. April 2020
+ * @author Markus Schnee, Jenny Glönkler, Felicia Wieland
+ * @version 25. April 2020
  */
 public class Stahl extends Eisenmetalle
 {
-   
     private double Kohlenstoffgehalt;
-    
+
     /**
-     * Konstruktor der abstrakten Klasse Stahl.
+     * Konstruktor der Klasse Stahl.
      * Die Superklasse Eisenmetalle wird aufgerufen, um Name, Nummer, Verwendung, elektrische Leitfähigkeit und Eisengehalt zu setzen, 
      * gegebenenfalls löst diese eine Exception aus.
      * Zudem wird die Methode SetKohlenstoffgehalt aufgerufen, um den Wert des Kohlenstoffgehalts zu setzen, 
@@ -22,42 +21,55 @@ public class Stahl extends Eisenmetalle
      * @param Name, Nummer, Verwendung, ElektrischeLeitfaehigkeit, Eisengehalt, Kohlenstoffgehalt
      * @throws Exception
      */
-    public Stahl(String Name,int Nummer,String Verwendung,double ElektrischeLeitfaehigkeit,double Eisengehalt, double Kohlenstoffgehalt) throws Exception
+    public Stahl(String Name,int Nummer,String Verwendung,double ElektrischeLeitfaehigkeit,double Eisengehalt,double Kohlenstoffgehalt) throws Exception
     {
-        super(Name,Nummer,Verwendung,ElektrischeLeitfaehigkeit ,Eisengehalt) ;
-        setKohlenstoffgehalt( Kohlenstoffgehalt);
+        super(Name,Nummer,Verwendung,ElektrischeLeitfaehigkeit,Eisengehalt) ;
+        setKohlenstoffgehalt(Kohlenstoffgehalt);
     }
-    
-     /**
-     * Setzt den Kohlenstoffgehalt auf den eingegebenen Wert, wenn dieser größer als 0 und kleiner als 2 ist. Ist das nicht der Fall, 
+
+    /**
+     * Standardkonstruktor der Klasse Stahl. Er ruft den Konstruktor der Superklasse Eisenmetalle auf und weist vorgegebene Werte zu.
+     * Außerdem wird dem Attribut Kohlenstoffgehalt ein Wert zugewiesen.
+     */
+    public Stahl() throws Exception
+    {
+        super("C45", 10503, "Maschinenbau", 8.3, 90);
+        setKohlenstoffgehalt(0.45);
+    }
+
+    /**
+     * Setzt den Kohlenstoffgehalt auf den eingegebenen Wert, wenn dieser größer als 0 und kleiner als 2.06 ist. Ist das nicht der Fall, 
      * wird eine Exception geworfen.
+     * 
      * @param Kohlenstoffgehalt
      * @throws Exception
      */
     public void setKohlenstoffgehalt(double Kohlenstoffgehalt)throws Exception
     {
-       if(Kohlenstoffgehalt>0 && Kohlenstoffgehalt< 2)
+        if(Kohlenstoffgehalt>0 && Kohlenstoffgehalt<= 2.06)
         {
             this.Kohlenstoffgehalt = Kohlenstoffgehalt;
-    
         }
         else
         {
             throw new Exception ("Der angegebene Kohlenstoffgehalt ist ungültig.");
         }
     }
+
     /**
      * Gibt den Wert des Kohlenstoffgehalts als double zurück.
+     * 
      * @return Kohlenstoffgehalt
      */
     public double getKohlenstoffgehalt()
     {
-       return Kohlenstoffgehalt;
+        return Kohlenstoffgehalt;
     }
-    
-     /**
-     * Sammelt die Eigenschaften eines Werkstoffes und speichert sie als String
-     * Das Sammeln geschieht, indem die Methode gibEigenschaft() aus der Klasse Eisenmetalle aufgerufen wird unter Verwendung des Schlüsselworts „super“
+
+    /**
+     * Sammelt die Eigenschaften eines Werkstoffes und speichert sie als String.
+     * Das Sammeln geschieht, indem die Methode gibEigenschaft() unter Verwendung des Schlüsselworts „super“ aus der nächsthöheren
+     * Klasse aufgerufen wird.
      * 
      * @return String mit den Eigenschaften des Werkstoffs: Name, Nummer, Verwendung, elektrische Leitfähigkeit, Eisengehalt, Kohlenstoffgehalt
      */
