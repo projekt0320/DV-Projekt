8ac5d971e6fea5910e7edea2f6d391e83cd199f4
Markus <m.schnee@hs-furtwangen.de>
2020-05-18 09:41:15 +0200

    gui

---
+++ Suchen_GUI2.java
@@ -0,0 +1,13 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Suchen_GUI2.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+import java.awt.*;
+public class Suchen_GUI2
+{
+    
+    
+}
